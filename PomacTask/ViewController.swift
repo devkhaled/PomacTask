115fff517bbc5bfc858c6f1c48620f3a09b9b4a7
Khaled <khaled1rashed@gmail.com>
2020-08-27 12:56:17 +0200

    Initial Commit

---
+++ PomacTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PomacTask
+//
+//  Created by Khaled Rashed on 8/27/20.
+//  Copyright © 2020 Khaled Rashed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
